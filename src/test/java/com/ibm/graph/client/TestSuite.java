bccad25d79c45174dd40b748aae5591f2a31044f
Patrick Titzler <ptitzler@us.ibm.com>
2016-12-17 14:04:11 -0800

    re-able all tests

--- src/test/java/com/ibm/graph/client/TestSuite.java
+++ src/test/java/com/ibm/graph/client/TestSuite.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-/*        GraphExceptionTests.class,      // tests class com.ibm.graph.client.exception.GraphException     
+        GraphExceptionTests.class,      // tests class com.ibm.graph.client.exception.GraphException     
         HTTPStatusInfoTests.class,      // tests class com.ibm.graph.client.response.HTTPStatusInfo
         GraphStatusInfoTests.class,     // tests class com.ibm.graph.client.response.GraphStatusInfo
         GraphResponseTests.class,       // tests class com.ibm.graph.client.response.GraphResponse
@@ -42,7 +42,7 @@ import java.util.Map;
         GremlinTests.class,             // tests gremlin specific methods in com.ibm.graph.client.IBMGraphClient
         GremlinPathTests.class,         // tests gremlin specific methods in com.ibm.graph.client.IBMGraphClient (path)
         GraphSONTests.class,            // tests graphSON load methods in com.ibm.graph.client.IBMGraphClient
-*/        GraphMLTests.class,             // tests graphML load methods in com.ibm.graph.client.IBMGraphClient
+        GraphMLTests.class,             // tests graphML load methods in com.ibm.graph.client.IBMGraphClient
         GraphMLTests2.class             // tests graphML load methods in com.ibm.graph.client.IBMGraphClient
 })
 public class TestSuite {
